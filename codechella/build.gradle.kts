b4ac72b2b2ee5f4637d60bcf89c0606f618e2b89
Douglas Prado <dc.prado@gmail.com>
2025-07-23 21:50:16 -0300

    01 - Criando uma API reativa

---
+++ codechella/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.3"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "br.com.alura"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(24)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+	// Flyway precisa de JDBC para funcionar
+	implementation("org.flywaydb:flyway-core")
+	implementation("org.flywaydb:flyway-database-postgresql")
+	implementation("org.springframework:spring-jdbc")
+
+	// Driver correto para R2DBC
+	runtimeOnly("io.r2dbc:r2dbc-postgresql:0.8.13.RELEASE")
+
+	// Driver JDBC necessário para Flyway
+	runtimeOnly("org.postgresql:postgresql:42.7.2")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.projectreactor:reactor-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
