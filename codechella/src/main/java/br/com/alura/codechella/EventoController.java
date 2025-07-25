1b447aedbc4e3e7ab45a26f4c373e4d737f7fb4b
Douglas Prado <dc.prado@gmail.com>
2025-07-25 03:20:06 -0300

    02 - Complementando a implementação

--- codechella/src/main/java/br/com/alura/codechella/EventoController.java
+++ codechella/src/main/java/br/com/alura/codechella/EventoController.java
@@ -2,20 +2,42 @@ package br.com.alura.codechella;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/eventos")
 public class EventoController {
 
     @Autowired
-    private EventoRepository repositorio;
+    private EventoService service;
 
-    @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<Evento> obterTodos() {
-        return repositorio.findAll();
+    @GetMapping //(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<EventoDto> obterTodos() {
+
+        return service.obterTodos();
+    }
+
+    @GetMapping("/{id}") //(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Mono<EventoDto> obterPorId(@PathVariable Long id) {
+
+        return service.obterPorId(id);
+    }
+
+    @PostMapping
+    public Mono<EventoDto> cadastrar(@RequestBody EventoDto dto){
+        return service.cadastrar(dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<Void> excluir(@PathVariable Long id) {
+        return service.excluir(id);
+
+    }
+
+    @PutMapping("/{id}")
+    public Mono<EventoDto> atualizar(@PathVariable Long id, @RequestBody EventoDto dto){
+        return service.atualizar(id, dto);
     }
 }
---
+++ codechella/src/main/java/br/com/alura/codechella/EventoService.java
@@ -0,0 +1,54 @@
+package br.com.alura.codechella;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class EventoService {
+
+    private EventoRepository repositorio;
+
+    public EventoService(EventoRepository repositorio) {
+        this.repositorio = repositorio;
+    }
+
+    public Flux<EventoDto> obterTodos() {
+        return  repositorio.findAll()
+                .map(EventoDto::toDto);
+    }
+
+    public Mono<EventoDto> obterPorId(Long id) {
+        return  repositorio.findById(id)
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND)))
+                .map(EventoDto::toDto);
+    }
+
+    public Mono<EventoDto> cadastrar(EventoDto dto) {
+
+        return repositorio.save(dto.toEntity())
+                .map(EventoDto::toDto);
+    }
+
+    public Mono<Void> excluir(Long id) {
+
+        return repositorio.findById(id)
+                .flatMap(repositorio::delete);
+    }
+
+    public Mono<EventoDto> atualizar(Long id, EventoDto dto) {
+
+        return repositorio.findById(id)
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Id do evento não encontrado.")))
+                .flatMap(eventoExistente -> {
+                    eventoExistente.setTipo(dto.tipo());
+                    eventoExistente.setNome(dto.nome());
+                    eventoExistente.setData(dto.data());
+                    eventoExistente.setDescricao(dto.descricao());
+                    return repositorio.save(eventoExistente);
+                })
+                .map(EventoDto::toDto);
+    }
+}
